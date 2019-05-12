656c1ad6f7d459d697f3359d79621314120d5bd5
sceat <sceat@hydre.io>
2019-05-12 20:28:06 +0300

    release

--- src/CFN.js
+++ src/CFN.js
@@ -1,41 +1,116 @@
 import AWS from 'aws-sdk'
 import { cache } from '@hydre/commons'
+import cfnStream from 'cfn-stack-event-stream'
+import states from './constant'
+import 'colors'
+const debug = require('debug')('cfn')
 
 export default class CFN {
-
 	constructor(key, secret, region) {
-		this.key = key
-		this.secret = secret
-		this.region = region
+		Object.assign(this, { key, secret, region })
 	}
 
-	async createOrUpdate(name, params, options = {}) {}
+	async createOrUpdate(name, template, params = [], options = {}) {
+		const alreadyExist = await this.#exist(name)
+		debug((alreadyExist ? 'Updating' : 'Creating') + ` stack [${name}]`)
+		try {
+			await new Promise((res, rej) => {
+				this.api[alreadyExist ? 'updateStack' : 'createStack'](
+					{
+						StackName: name,
+						TemplateBody: JSON.stringify(template),
+						Parameters: params,
+						...options
+					},
+					(err, data) => {
+						if (err) rej(err)
+						else res(data)
+					}
+				)
+			})
+			cfnStream(this.api, name)
+				.on(
+					'data',
+					({
+						Timestamp,
+						ResourceType,
+						LogicalResourceId,
+						ResourceStatus,
+						ResourceStatusReason
+					}) => {
+						debug.extend(LogicalResourceId)(
+							`[${ResourceType}] ${states[ResourceStatus]} ${
+								ResourceStatusReason ? `(${ResourceStatusReason})` : ''
+							}`
+						)
+					}
+				)
+				.on('end', async () => {
+					debug.extend(name)(`was ${alreadyExist ? 'updated' : 'created'} ${'successfully'.green}`)
+				})
+		} catch ({ message }) {
+			debug.extend('err')(message)
+		}
+	}
 
 	async delete(StackName) {
 		return new Promise((res, rej) => {
+			debug(`Deleting stack ${StackName}`)
 			this.api.deleteStack({ StackName }, (err, data) => {
 				if (err) rej(err)
-				res(data)
+				else {
+					cfnStream(this.api, StackName)
+						.on(
+							'data',
+							({
+								Timestamp,
+								ResourceType,
+								LogicalResourceId,
+								ResourceStatus,
+								ResourceStatusReason = ''
+							}) => {
+								debug.extend(LogicalResourceId)(
+									`[${ResourceType}] ${states[ResourceStatus]} ${
+										ResourceStatusReason ? `(${ResourceStatusReason})` : ''
+									}`
+								)
+							}
+						)
+						.on('end', async () => {
+							debug.extend(StackName)(`was deleted ${'successfully'.green}`)
+						})
+				}
 			})
 		})
 	}
 
-	async estimateCost(template, params = [], options = {}) {
+	async validate(template) {
+		const TemplateBody = JSON.stringify(template)
 		return new Promise((res, rej) => {
-			this.api.estimateTemplateCost(
-				{
-					TemplateBody: template,
-					Parameters: params
-				},
-				(err, data) => {
-					if (err) rej(err)
-					res(data)
-				}
-			)
+			this.api.validateTemplate({ TemplateBody }, (err, data) => {
+				if (err) rej(false)
+				else res(true)
+			})
 		})
 	}
 
-	async #describe() {}
+	async #describe(StackName) {
+		return new Promise((res, rej) => {
+			this.api.describeStacks({ StackName }, (err, data) => {
+				if (err) rej(err)
+				else res(data)
+			})
+		})
+	}
+
+	async #exist(StackName) {
+		try {
+			await this.#describe(StackName)
+			return true
+		} catch (error) {
+			return false
+		}
+	}
 
 	@cache // memoize to instanciate the api only once
 	get api() {
@@ -48,9 +123,6 @@ export default class CFN {
 	}
 }
 
-export function param(key, val) {
-	return {
-		ParameterKey: key,
-		ParameterValue: val
-	}
+export function param(ParameterKey, ParameterValue) {
+	return { ParameterKey, ParameterValue }
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 export { default } from './CFN'
+export * from './CFN'
