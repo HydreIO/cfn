9ab7d0116d4faedb312e697be3cec9bc8db18895
sceat <sceat@hydre.io>
2019-05-16 18:16:20 +0300

    babel config

--- babel.config.js
+++ babel.config.js
@@ -12,8 +12,9 @@ module.exports = {
   plugins: [
 		['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ["@babel/plugin-proposal-private-methods", { "loose": true }],
+    ["@babel/plugin-proposal-private-methods", { loose: true }],
 		['@babel/plugin-proposal-function-bind'],
-		['@babel/plugin-proposal-optional-chaining']
+    ['@babel/plugin-proposal-optional-chaining'],
+    ["@babel/plugin-proposal-export-default-from"]
 	]
 }
